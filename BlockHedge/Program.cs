c83119c94d5b2b046df4fcaa19724b695b530b73
Alain Schaerer <aschaerer@pcatg.com>
2024-10-20 23:31:46 -0400

    Need to figure out why proposal id is 1 instead of 0 for the first proposal

--- BlockHedge/Program.cs
+++ BlockHedge/Program.cs
@@ -5,21 +5,41 @@ using BlockHedge.Services;
 using Nethereum.UI;
 using Nethereum.Metamask;
 using Nethereum.Metamask.Blazor;
+using Microsoft.AspNetCore.Components.Authorization;
+using Nethereum.Blazor;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddAuthorizationCore();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register IEthereumHostProvider
+builder.Services.AddSingleton<IEthereumHostProvider, MetamaskHostProvider>();
+
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
+builder.Services.AddSingleton<MetamaskInterceptor>();
+builder.Services.AddSingleton<MetamaskHostProvider>();
+
+//Add metamask as the selected ethereum host provider
+builder.Services.AddSingleton(services =>
+{
+    var metamaskHostProvider = services.GetService<MetamaskHostProvider>();
+    var selectedHostProvider = new SelectedEthereumHostProviderService();
+    selectedHostProvider.SetSelectedEthereumHostProvider(metamaskHostProvider);
+    return selectedHostProvider;
+});
+
+
+builder.Services.AddSingleton<AuthenticationStateProvider, EthereumAuthenticationStateProvider>();
 
 // Register VotingContractService
 builder.Services.AddSingleton<VotingContractService>(sp => 
     new VotingContractService("0x9190428F46c1c6eB765dc7F7169eF3470eccd903")
 );
 
-// Register IEthereumHostProvider
-builder.Services.AddSingleton<IEthereumHostProvider, MetamaskHostProvider>();
 
 // Register WalletService
 builder.Services.AddScoped<WalletService>();
